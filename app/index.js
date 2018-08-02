d151716df215e88d4306699248ed509bd000c9d1
Alan Palazzolo <alan.palazzolo@startribune.com>
2018-08-02 15:42:50 -0500

    Component-izing and hooking up API.

--- app/index.js
+++ app/index.js
@@ -1,9 +1,36 @@
-import Map from './map.js';
+// Utilize templates on the client.  Get the main content template.
+import Content from '../templates/_index-content.svelte.html';
 
-(function(){
-  let map_dem = new Map('#d-gov-map');
-  map_dem.render();
+// Create UI
+const app = new Content({
+  target: document.querySelector('.article-lcd-body-content'),
+  hydrate: true,
+  data: {}
+});
 
-  let map_rep = new Map('#r-gov-map');
-  map_rep.render();
-})();
+window._app = app;
+
+// // Hacky, get share elements
+// $(document).ready(() => {
+//   let $share = $('.share-placeholder').size()
+//     ? $('.share-placeholder')
+//       .children()
+//       .detach()
+//     : undefined;
+//   let attachShare = !$share
+//     ? undefined
+//     : () => {
+//       $('.share-placeholder').append($share);
+//     };
+
+// });
+
+// import Map from './map.js';
+
+// (function() {
+//   let map_dem = new Map('#d-gov-map');
+//   map_dem.render();
+
+//   let map_rep = new Map('#r-gov-map');
+//   map_rep.render();
+// })();
