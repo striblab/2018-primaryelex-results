30098f6ca62e08719d59bd2aaa6f55fc2f9600d6
Alan Palazzolo <github@zzolo.org>
2018-08-07 00:05:04 -0500

    Updating for data restructure.

--- app/index.js
+++ app/index.js
@@ -1,13 +1,23 @@
 // Utilize templates on the client.  Get the main content template.
 import Content from '../templates/_index-content.svelte.html';
+import { Store } from 'svelte/store.js';
+
+// Create global store for UI
+const store = new Store({
+  // No trailing slash
+  civixEndpoint: '//static.startribune.com/elections/civix-test/mn-20180814'
+});
 
 // Create UI
 const app = new Content({
   target: document.querySelector('.article-lcd-body-content'),
   hydrate: true,
-  data: {}
+  data: {},
+  store
 });
 
+// Debug
+window._store = store;
 window._app = app;
 
 // // Hacky, get share elements
