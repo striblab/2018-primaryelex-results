99fbd8db29c1348d16571a1bc972fc8142c799b0
Alan Palazzolo <alan.palazzolo@startribune.com>
2018-08-08 11:41:55 -0500

    Adding precinct reporting styles to map.

--- app/index.js
+++ app/index.js
@@ -5,52 +5,44 @@ import supplement from '../assets/data/results-supplement.json';
 import { feature as topojsonFeature } from 'topojson';
 import mnCountiesTopo from '../sources/mncounties.json';
 
-// Create global store for UI
-const store = new Store({
-  // No trailing slash
-  civixEndpoint: '//static.startribune.com/elections/civix-test/mn-20180814',
-  interval: 60 * 1000,
-  mnCountiesGeo: topojsonFeature(
-    mnCountiesTopo,
-    mnCountiesTopo.objects.counties
-  ).features
-});
-
-// Create UI
-const app = new Content({
-  target: document.querySelector('.article-lcd-body-content'),
-  hydrate: true,
-  data: {
-    supplement
-  },
-  store
-});
-
-// Debug
-window._store = store;
-window._app = app;
+/* global $ */
 
 // // Hacky, get share elements
-// $(document).ready(() => {
-//   let $share = $('.share-placeholder').size()
-//     ? $('.share-placeholder')
-//       .children()
-//       .detach()
-//     : undefined;
-//   let attachShare = !$share
-//     ? undefined
-//     : () => {
-//       $('.share-placeholder').append($share);
-//     };
+$(document).ready(() => {
+  let $share = $('.share-placeholder').size()
+    ? $('.share-placeholder')
+      .children()
+      .detach()
+    : undefined;
+  let attachShare = !$share
+    ? undefined
+    : () => {
+      $('.share-placeholder').append($share);
+    };
 
-// });
+  // Create global store for UI
+  const store = new Store({
+    // No trailing slash
+    civixEndpoint: '//static.startribune.com/elections/civix-test/mn-20180814',
+    interval: 60 * 1000,
+    mnCountiesGeo: topojsonFeature(
+      mnCountiesTopo,
+      mnCountiesTopo.objects.counties
+    ).features
+  });
 
-// import Map from './map.js';
+  // Create UI
+  const app = new Content({
+    target: document.querySelector('.article-lcd-body-content'),
+    hydrate: true,
+    data: {
+      supplement,
+      attachShare
+    },
+    store
+  });
 
-// (function() {
-//   let map_dem = new Map('#d-gov-map');
-//   map_dem.render();
-
-//   let map_rep = new Map('#r-gov-map');
-//   map_rep.render();
-// })();
+  // Debug
+  window._store = store;
+  window._app = app;
+});
