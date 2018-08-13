b7c052277e53cc5e92d1ee6778f6441dc55e7aa9
Alan Palazzolo <alan.palazzolo@startribune.com>
2018-08-13 16:49:14 -0500

    Ugh...

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,30 @@ let loaders = {
   babel: {
     loader: 'babel-loader',
     options: {
-      cacheDirectory: true
+      cacheDirectory:
+        process.env.NODE_ENV === 'development' || process.env.DEBUG
+          ? false
+          : true,
+      presets: [
+        // Use debug: true to see what transforms are happening
+        // and what browsers needed them
+        [
+          '@babel/preset-env',
+          {
+            debug: true,
+            targets: { browsers: ['ie >= 7'] }
+          }
+        ]
+      ],
+      plugins: [
+        'lodash',
+        [
+          '@babel/plugin-transform-runtime',
+          {
+            regenerator: true
+          }
+        ]
+      ]
     }
   },
   svelte: {
@@ -26,22 +49,20 @@ let loaders = {
 module.exports = {
   mode: process.env.NODE_ENV || 'production',
   devtool: 'source-map',
-  entry: './app/index.js',
+  entry: './templates/_index-content.svelte.html',
   output: {
     path: path.resolve(__dirname, './build'),
-    filename: 'app.bundle.js'
+    filename: 'test-content.bundle.js'
   },
   module: {
     rules: [
       // Loaded top to bottom.  Right to left.
       {
-        test: /\.(svelte\.html|svelte)$/,
-        exclude: /node_modules/,
+        test: /.*\.(svelte\.html|svelte)$/,
         use: [loaders.babel, loaders.svelte]
       },
       {
-        test: /app.*\.js$/,
-        exclude: /node_modules/,
+        test: /.*\.(js)$/,
         use: [loaders.babel]
       }
     ]
@@ -52,6 +73,7 @@ module.exports = {
         sourceMap: true,
         uglifyOptions: {
           ecma: 5,
+          ie8: true,
           compress: true,
           safari10: true,
           mangle: {
